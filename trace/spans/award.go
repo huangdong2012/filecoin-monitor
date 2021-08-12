a047f961fcbee02577c003b9985db034c1f04003
黄东 <shiliyuanma@gmail.com>
2021-08-12 12:50:58 +0800

    update span: award

--- trace/spans/award.go
+++ trace/spans/award.go
@@ -15,6 +15,10 @@ type AwardSpan struct {
 	*StatusSpan
 }
 
+func (s *AwardSpan) SetSystemActorAddr(addr string) {
+	s.AddAttributes(trace.StringAttribute("system_actor_addr", addr))
+}
+
 func (s *AwardSpan) SetMinerID(mid string) {
 	s.AddAttributes(trace.StringAttribute("miner_id", mid))
 }
@@ -23,14 +27,26 @@ func (s *AwardSpan) SetEpoch(epoch int64) {
 	s.AddAttributes(trace.Int64Attribute("epoch", epoch))
 }
 
-func (s *AwardSpan) SetPenalty(p int64) {
-	s.AddAttributes(trace.Int64Attribute("penalty", p))
+func (s *AwardSpan) SetCID(cid string) {
+	s.AddAttributes(trace.StringAttribute("cid", cid))
 }
 
-func (s *AwardSpan) SetReward(r int64) {
-	s.AddAttributes(trace.Int64Attribute("reward", r))
+func (s *AwardSpan) SetMsgCount(count int) {
+	s.AddAttributes(trace.Int64Attribute("msg_count", int64(count)))
 }
 
-func (s *AwardSpan) SetWinCount(count int) {
-	s.AddAttributes(trace.Int64Attribute("win_count", int64(count)))
+func (s *AwardSpan) SetBaseFee(fee uint64) {
+	s.AddAttributes(trace.Int64Attribute("base_fee", int64(fee)))
+}
+
+func (s *AwardSpan) SetPenalty(p uint64) {
+	s.AddAttributes(trace.Int64Attribute("penalty", int64(p)))
+}
+
+func (s *AwardSpan) SetReward(r uint64) {
+	s.AddAttributes(trace.Int64Attribute("reward", int64(r)))
+}
+
+func (s *AwardSpan) SetWinCount(count int64) {
+	s.AddAttributes(trace.Int64Attribute("win_count", count))
 }
