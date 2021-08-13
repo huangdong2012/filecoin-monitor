983e8e1374a38c5b9c518ab0464f92deacaab0e4
黄东 <shiliyuanma@gmail.com>
2021-08-13 18:07:39 +0800

    update worker enum

--- model/enum.go
+++ model/enum.go
@@ -3,11 +3,13 @@ package model
 type PackageKind string
 
 const (
-	PackageKind_Lotus   PackageKind = "lotus"
-	PackageKind_Miner   PackageKind = "miner"
-	PackageKind_Worker  PackageKind = "worker"
-	PackageKind_Storage PackageKind = "storage"
-	PackageKind_Agent   PackageKind = "agent"
+	PackageKind_Lotus        PackageKind = "lotus"
+	PackageKind_Miner        PackageKind = "miner"
+	PackageKind_Worker       PackageKind = "worker"
+	PackageKind_WorkerWdPost PackageKind = "worker_wdpost"
+	PackageKind_WorkerWnPost PackageKind = "worker_wnpost"
+	PackageKind_Storage      PackageKind = "storage"
+	PackageKind_Agent        PackageKind = "agent"
 )
 
 type NodeStatus int32
